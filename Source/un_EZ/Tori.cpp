ff0ba2bda9135ee66957e44819b08eecdbc7b4e5
Audun - Skole PC <audunking@gmail.com>
2019-01-17 12:46:11 +0100

    Set up movement(Rotation functions)
    Rotation is set up and working.
    Remember to find way know the controllers direction (so you can flick the controller to attack in direction)

--- Source/un_EZ/Tori.cpp
+++ Source/un_EZ/Tori.cpp
@@ -1,12 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tori.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Engine/GameEngine.h"
 
 // Sets default values
 ATori::ATori()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+
 
 }
 
@@ -21,6 +31,11 @@ void ATori::BeginPlay()
 void ATori::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	UE_LOG(LogTemp, Warning, TEXT("Controllers's Rotation is %s"),
+		*GetControlRotation().ToString());
+
+	UE_LOG(LogTemp, Warning, TEXT("MyCharacter's ForwardVector is %s"),
+		*GetActorForwardVector().ToString());
 
 }
 
@@ -29,5 +44,46 @@ void ATori::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	InputComponent->BindAxis("Move_X", this, &ATori::move_X);
+	InputComponent->BindAxis("Move_Y", this, &ATori::move_Y);
+
+
+}
+
+void ATori::move_X(float axisValue)
+{
+	/*
+	if ((GetController() != NULL) && (axisValue != 0.0f))
+	{
+		// find out which way is forward
+		const FRotator Rotation = GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0); 
+		//found this easier than other methods since it just adds a float value, and can be changed into any direction.
+
+		// get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, axisValue);
+		
+	}	
+	*/
+	AddMovementInput(FVector(1, 0.f, 0.f), axisValue);
+}
+
+void ATori::move_Y(float axisValue)
+{
+	/*
+	if ((GetController() != NULL) && (axisValue != 0.0f))
+	{
+		// find out which way is right
+		const FRotator Rotation = GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		// get right vector 
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// add movement in that direction
+		AddMovementInput(Direction, axisValue);
+		
+	}
+	*/
+	AddMovementInput(FVector(0.f, 1, 0.f), axisValue);
 }
 
--- Source/un_EZ/Tori.h
+++ Source/un_EZ/Tori.h
@@ -26,4 +26,7 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void move_X(float axisValue);
+	void move_Y(float axisValue);
+
 };
