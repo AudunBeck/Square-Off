9a435a896b630a7b866abfd7f5ac15bc311d3fcc
Audun - Skole PC <audunking@gmail.com>
2019-01-21 13:19:39 +0100

    Added RockAbility 2
    Makes a wall, it is destroyed with UE4's lifespan.
    Scale, life and range is handled by the Rock Element class.

--- Source/un_EZ/RockElement.h
+++ Source/un_EZ/RockElement.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "BaseElement.h"
+#include "RockElementAbility2.h"
 #include "RockElement.generated.h"
 
 /**
@@ -14,10 +15,21 @@ class UN_EZ_API ARockElement : public ABaseElement
 {
 	GENERATED_BODY()
 
-
 public:
 
 	virtual void ability1()override;
 	virtual void ability2()override;
+
+	UPROPERTY(EditAnywhere, Category = "Ability2")
+		float ability2Range;
+	UPROPERTY(EditAnywhere, Category = "Ability2")
+		FVector ability2Scale;
+	UPROPERTY(EditAnywhere, Category = "Ability2")
+		float ability2Lifespan = 2;
+	
+
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<class ARockElementAbility2> wallBlueprint;
 	
 };
