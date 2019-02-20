dada5b8745d000468ca48e047469af81c7d8f388
Beck <audunsbeck@hotmail.com>
2019-02-20 16:32:03 +0100

    Added new animations to fire and new walk /run

--- Source/un_EZ/FireElement.cpp
+++ Source/un_EZ/FireElement.cpp
@@ -6,7 +6,7 @@
 
 AFireElement::AFireElement()
 {
-	
+
 	static ConstructorHelpers::FObjectFinder<UDataTable>
 		FireElementTable(TEXT("DataTable'/Game/DataTables/FireElementTable.FireElementTable'"));
 	BalancingTable = FireElementTable.Object;
@@ -46,7 +46,7 @@ void AFireElement::ability1()
 	{
 		Super::ability1();
 	}
-	
+
 }
 
 void AFireElement::ability1End()
@@ -54,17 +54,17 @@ void AFireElement::ability1End()
 	//UE_LOG(LogTemp, Warning, TEXT("FireElement Ability 1 firing"));
 
 // Dash part of the attack
-	
-		myOwner->LaunchCharacter(myOwner->GetActorForwardVector() * firePunch, false, true);
-		FActorSpawnParameters tempParam;
-		tempParam.Owner = this;
-		AFireElementAbility1* temp;
-		temp = GetWorld()->SpawnActor<AFireElementAbility1>(FireElementAbility1_BP,
-			myOwner->GetActorLocation() + myOwner->GetActorForwardVector() * ability1Range, myOwner->GetActorRotation(), tempParam);
-		if (fireChi > 0)
-		{
-			fireChi -= 1;
-		}
+
+	myOwner->LaunchCharacter(myOwner->GetActorForwardVector() * firePunch, false, true);
+	FActorSpawnParameters tempParam;
+	tempParam.Owner = this;
+	AFireElementAbility1* temp;
+	temp = GetWorld()->SpawnActor<AFireElementAbility1>(FireElementAbility1_BP,
+		myOwner->GetActorLocation() + myOwner->GetActorForwardVector() * ability1Range, myOwner->GetActorRotation(), tempParam);
+	if (fireChi > 0)
+	{
+		fireChi -= 1;
+	}
 }
 
 void AFireElement::ability2()
@@ -73,29 +73,25 @@ void AFireElement::ability2()
 	{
 		Super::ability2();
 	}
-	
+
 }
 
 void AFireElement::ability2End()
 {
-	if (ammo2 > 0)
-	{
+	//UE_LOG(LogTemp, Warning, TEXT("FireElement Ability 2 firing"));
+	myOwner->LaunchCharacter(myOwner->GetActorForwardVector() * fireKick, false, true);
 
-		//UE_LOG(LogTemp, Warning, TEXT("FireElement Ability 2 firing"));
-		myOwner->LaunchCharacter(myOwner->GetActorForwardVector() * fireKick, false, true);
+	FActorSpawnParameters tempParam;
+	tempParam.Owner = this;
+	AFireElementAbility2* temp;
+	temp = GetWorld()->SpawnActor<AFireElementAbility2>(FireElementAbility2_BP,
+		myOwner->GetActorLocation() + myOwner->GetActorForwardVector() * ability2Range, myOwner->GetActorRotation(), tempParam);
 
-		FActorSpawnParameters tempParam;
-		tempParam.Owner = this;
-		AFireElementAbility2* temp;
-		temp = GetWorld()->SpawnActor<AFireElementAbility2>(FireElementAbility2_BP,
-			myOwner->GetActorLocation() + myOwner->GetActorForwardVector() * ability2Range, myOwner->GetActorRotation(), tempParam);
+	//temp->setupAttack(myOwner, ability1lifeSpan, ability2Range);
 
-		//temp->setupAttack(myOwner, ability1lifeSpan, ability2Range);
-
-		// Refills ammo1 as mentioned in design doc
-		ammo1 += ammo1Refill;
-		if (ammo1 > maxAmmo1)
-			ammo1 = maxAmmo1;
-		fireChi = maxFireChi;
-	}
+	// Refills ammo1 as mentioned in design doc
+	ammo1 += ammo1Refill;
+	if (ammo1 > maxAmmo1)
+		ammo1 = maxAmmo1;
+	fireChi = maxFireChi;
 }
