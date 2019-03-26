73ab23e731323fa2fd0fdd644d5b964b009c0b75
Morgan <morganchane@live.no>
2019-03-26 10:27:02 +0100

    morning commit

--- Source/un_EZ/BaseAbility.cpp
+++ Source/un_EZ/BaseAbility.cpp
@@ -25,7 +25,13 @@ ATori* ABaseAbility::getMyOwner()
 
 void ABaseAbility::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Gaining point"));
-	myPlayer->gainPoint();
+	if (OtherActor != myPlayer)
+	{
+		if (OtherActor->IsA(ATori::StaticClass()))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Gaining point"));
+			myPlayer->gainPoint();
+		}
+	}
 }
 
